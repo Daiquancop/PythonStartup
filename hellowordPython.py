7b68789136880c95d39d13af89be3397c3c94512
long217 <long2171997@gmail.com>
2020-04-03 21:18:39 +0700

    Start-up day 1

--- hellowordPython.py
+++ hellowordPython.py
@@ -1,1 +1,15 @@
-print('Hello !')
+def sumFunction(a, b):
+    return a + b
+
+def intTryParse(str):
+    try:
+        return int(str)
+    except ValueError:
+        return 0
+
+x = input('Nhap x: ')
+x = intTryParse(x)
+y = input('Nhap y: ')
+y = intTryParse(y)
+
+print(sumFunction(x, y))
